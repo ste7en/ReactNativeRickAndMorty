a06164702bd2d9f62ff0de2667c08afa43ea6a21
stefano.formicola <stefano.formicola@mia-care.io>
2022-07-05 23:05:13 +0200

    src dir && App.tsx moved

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,10 @@
 
 import 'react-native'
 import React from 'react'
-import App from '../App'
+import App from '../src/App'
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer'
+import {render} from '@testing-library/react-native'
 
 it('renders correctly', () => {
-  renderer.create(<App />)
+  render(<App />)
 })
